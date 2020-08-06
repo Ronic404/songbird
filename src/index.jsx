d9df5ee544693bb6cabdd7081e3b01db6ad87ac6
ROnic404 <ronic@inbox.ru>
2020-08-06 21:08:53 +0400

    feat: add image and audio

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,9 @@
-/* eslint-disable react/destructuring-assignment */
+/* eslint-disable react/no-did-update-set-state */
+/* eslint-disable react/no-access-state-in-setstate */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { hot } from 'react-hot-loader';
-// import birdsData from './js/data/birdsData';
+import birdsData from './js/data/birdsData';
 
 import Header from './js/components/Header/Header';
 import QuestionBlock from './js/components/QuestionBlock/QuestionBlock';
@@ -18,23 +19,35 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      page: 0,
       score: 0,
       points: 5,
+      page: 0,
+      randomIndex: 0,
     }
     this.setNextPage = this.setNextPage.bind(this);
+    
   }
+
+  componentDidMount() {
+    const { page } = this.state;
+    this.setState({ randomIndex: Math.floor(Math.random() * birdsData[page].length) })
+  } 
+
+  // componentDidUpdate(prevState) {
+  //   if (this.state.page !== prevState.page) {
+  //     this.setState({ randomIndex: Math.floor(Math.random() * birdsData[this.state.page].length) })
+  //   }  
+  // }
 
   setNextPage() {
     const { page } = this.state;
-
+    this.addPoints();
     if (page === 5) {
       this.setState({ page: 0});
+      this.setState({ score: 0});
     } else {
       this.setState({ page: page + 1});
     }
-
-    this.addPoints();
   }
 
   addPoints() {
@@ -44,16 +57,13 @@ class App extends React.Component {
   }
 
   render() {
-    const { page, score } = this.state;
-    // function nameBird() {
-    //   return birdsData[this.state.page][Math.floor(Math.random() * birdsData[this.state.page].length)]
-    // }
-    // console.log(nameBird())
+    const { page, score, randomIndex } = this.state;
+    window.console.log(randomIndex)
     return (
       <>
         <Header page={page} score={score} />
-        <QuestionBlock />
-        <AnswerBlock page={page}/>
+        <QuestionBlock page={page} randomIndex={randomIndex} />
+        <AnswerBlock page={page} randomIndex={randomIndex} />
         <Button onClick={this.setNextPage} />
       </>
     );
@@ -62,3 +72,15 @@ class App extends React.Component {
 
 const AppWithHot = hot(module)(App);
 ReactDOM.render(<AppWithHot name="Jane" />, document.querySelector('#root'));
+
+
+
+
+  // function nameBird() {
+  //   return birdsData[this.state.page][Math.floor(Math.random() * birdsData[this.state.page].length)]
+  // }
+
+  // randomIndex() {
+  // this.setState({ aaa: Math.floor(Math.random() * birdsData[this.state.page].length) })
+  // return Math.floor(Math.random() * birdsData[this.state.page].length);
+  // }
--- src/js/components/QuestionBlock/QuestionBlock.jsx
+++ src/js/components/QuestionBlock/QuestionBlock.jsx
@@ -1,15 +1,18 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable jsx-a11y/media-has-caption */
 import React from 'react';
-import bird from '../../../assets/images/bird.png'
+import birdsData from '../../data/birdsData';
+// import bird from '../../../assets/images/bird.png';
 
-export default function QuestionBlock() {
+export default function QuestionBlock(props) {
+  const { page, randomIndex } = props;
+
   return (
     <div className="question-block"> 
-      <img className="question-block__image" src={bird} alt="qweqwe" />
+      <img className="question-block__image" src={birdsData[page][randomIndex].image} alt="Bird" />
       <div className="question-block__description">
         <h2 className="question-block__description-title">******</h2>
-        <audio className="question-block__description-audio" controls>
-          <track kind="captions" />
-        </audio>
+        <audio className="question-block__description-audio" src={birdsData[page][randomIndex].audio} controls />
       </div>
     </div>
   );
